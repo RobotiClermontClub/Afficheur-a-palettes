e56d25e685f408d3533c390c75593a2a6ce50f63
WilsonUCA <wilson.duguet@etu.uca.fr>
2025-02-26 17:07:31 +0100

    V0.2

--- Split_Flap_rotation_control.ino
+++ Split_Flap_rotation_control.ino
@@ -6,16 +6,18 @@ int phase3 = 5;
 int phase4 = 6;
 
 int hallSensor = 2;
-int interruptOn;
 
 int stepsPerRevolution = 2048;
 // 64 pas = 2048 micro-pas réels
 // 2048 / 11 emplacements = 186.1818 micro-pas par emplacement = steps a effectuer
-// 372.36 / 64 = 2.905 tours stepper par empl.
-int gearbox = 64;
-int motorSpeed = 8; // en RPM
+//int gearbox = 32;
+int motorSpeed = 8; // en RPM, 8 max sinon probleme de drive
 int stepCount = 0;
+int originOffset = 120; // A REMODIFIER AVEC LES PIECES QUI ONT MOINS DE BACKLASH
 
+int resetPin = 8;
+
+/*
 // Liste des caractères à display
 char caracteres[] = {
     // Lettres majuscules 0-25
@@ -31,8 +33,29 @@ char caracteres[] = {
     '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', 
     '_', '`', '{', '|', '}', '~'
 };
+*/
 
-int stepsPerLetters[] = {0,0,186,186*2,186*3,186*4,186*5,186*6,186*7,186*8,186*9};
+char caracteres[] = {
+    // endMarkers
+    '\n','\r',
+    // Chiffres 0-9
+    ' ','9', '8', '7', '6', '5', '4', '3', '2', '1', '0'
+
+};
+
+int stepsPerLetters[] = {
+    0, 0, 0 + originOffset,
+    186 + 1 + originOffset,
+    186 * 2 + originOffset,
+    186 * 3 + originOffset,
+    186 * 4 + originOffset,
+    186 * 5 + originOffset,
+    186 * 6 + 1 + originOffset,
+    186 * 7 + originOffset,
+    186 * 8 + originOffset,
+    186 * 9 + originOffset,
+    186 * 10 + originOffset
+};
 
 int size = sizeof(caracteres) / sizeof(caracteres[0]);
 
@@ -61,6 +84,7 @@ void setup() {
   // Sensor
   pinMode(hallSensor, INPUT_PULLUP);
   //attachInterrupt(digitalPinToInterrupt(hallSensor), interruptHandler, FALLING);
+  pinMode(resetPin, INPUT_PULLUP);
 
   //Communication
   Serial.begin(9600);
@@ -76,11 +100,15 @@ void loop() {
   //moteur.step(2048);
   //delay(1000);
 
-  // Rearmemant interruption
+  // Reset
+  if (digitalRead(resetPin) == LOW) {
+    homeSequence();
+  }
 
   /*
   Communication IHM
   */
+
   char c;
   if (Serial.available() > 0) {
     // Lire le caractère envoyé par l'utilisateur
@@ -91,16 +119,13 @@ void loop() {
       Serial.println(c);
 
       ToLetter(toSteps(c));
-      //moteur.step(1);
     }
   }
-
-  /*
-  Interruption
-  */
-
-
 }
+
+/*
+FONCTIONS
+*/
 
 void ContinuousRotation() {
   // Rotation continue jusqu'a reset de la carte Arduino
@@ -127,16 +152,16 @@ void ToLetter(int steps) {
     }
   if (diff > 0) {
     stepCount += diff;
-    Serial.println(stepCount);
-    Serial.println(diff);
     moteur.step(diff);
     }
-  if (diff < 0)  {
-    Serial.println(stepCount);
-    Serial.println(diff);
-    stepCount += diff;
-    moteur.step(stepsPerRevolution + diff);
+  if (diff < 0)  { 
+    //stepCount += diff;
+    //moteur.step(stepsPerRevolution + diff);
+    homeSequence();
+    stepCount += steps-originOffset;
+    moteur.step(steps-originOffset);
     }
+  
 }
 
 void homeSequence() {
@@ -144,12 +169,21 @@ void homeSequence() {
   if (digitalRead(hallSensor) == LOW) {
     moteur.step(500);
   }
-  while (digitalRead(hallSensor) != LOW) {
+  while (digitalRead(hallSensor) != LOW) { // AJOUTER UNE ERREUR APRéS UN CERTAIN TEMPS
     moteur.step(1);
-    Serial.print(stepCount);
   }
-  stepCount = 0;
+  stepCount = originOffset;
+  moteur.step(originOffset);
+
+  
 }
+
+
+/*
+INTERRUPTIONS
+*/
+
+
 
 /*
 INUTILISé
@@ -157,10 +191,11 @@ INUTILISé
 
 // interrupt on hall effect front
 void interruptHandler() {
-  Serial.print("AIMANT !!  '\n'");
+  if (stepCount > 1000) {
+
+  }
   detachInterrupt(digitalPinToInterrupt(hallSensor));
 }
 // set step count to 0 -> position initial
-
-
 */
+
